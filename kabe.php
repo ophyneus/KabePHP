34d33b7d19b4b1cd8e8d8613848237f8d07bc81a
Ophyneus <ophyneus@tlu.ee>
2016-09-22 20:54:11 +0300

    Move checks

--- kabe.php
+++ kabe.php
@@ -14,7 +14,12 @@
 	  private $aktiivseveerg = -1;
     private $viimaneNupp = "v";
 	  function html(){
-		  $t="<table>";
+      if($this->viimaneNupp == "v"){
+        $t="<h1>Musta kord</h1>";
+      }elseif ($this->viimaneNupp == "m") {
+        $t="<h1>Valge kord</h1>";
+      }
+		  $t.="<table>";
 		  for($rida=0; $rida<8; $rida++){
 			  $t.="\n<tr>";
 			  for($veerg=0; $veerg<8; $veerg++){
@@ -23,7 +28,7 @@
 					  $stiil.="background-color: lightgray;";
 				  }
 				  if($rida==$this->aktiivserida && $veerg==$this->aktiivseveerg){
-					  $stiil.="font-weight: bold;";
+					  $stiil.="font-weight: bold; background-color: red;";
 				  }
 				  $lahter="&nbsp;";
 				  if(in_array($this->laud[$rida][$veerg], ["m"])){
@@ -47,28 +52,52 @@
 	  }
 	  function k2ik($uusrida, $uusveerg){
 		  $nupp=$this->laud[$this->aktiivserida][$this->aktiivseveerg];
-      if($nupp == "m" && $this->viimaneNupp == "v" && $this->aktiivserida - $uusrida == 1 &&
-        ($this->aktiivseveerg - $uusveerg == 1 || $this->aktiivseveerg - $uusveerg == -1)){
+      if($nupp == "m" && $this->viimaneNupp == "v" && $this->laud[$uusrida][$uusveerg] != "v" && $this->laud[$uusrida][$uusveerg] != "m" &&
+        $this->aktiivserida - $uusrida == 1 && ($this->aktiivseveerg - $uusveerg == 1 || $this->aktiivseveerg - $uusveerg == -1)){
             $this->laud[$uusrida][$uusveerg]=$nupp;
             //echo $uusrida, $uusveerg, $nupp;
             $this->laud[$this->aktiivserida][$this->aktiivseveerg]="r";
             $this->aktiivserida=-1;
             $this->aktiivseveerg=-1;
-      }else if ($nupp == "v" && $this->viimaneNupp == "m" && $this->aktiivserida - $uusrida == -1 &&
-         ($this->aktiivseveerg - $uusveerg == 1 || $this->aktiivseveerg - $uusveerg == -1)){
+            $this->viimaneNupp=$nupp;
+      }else if($nupp == "v" && $this->viimaneNupp == "m" && $this->laud[$uusrida][$uusveerg] != "m" && $this->laud[$uusrida][$uusveerg] != "v" &&
+         $this->aktiivserida - $uusrida == -1 && ($this->aktiivseveerg - $uusveerg == 1 || $this->aktiivseveerg - $uusveerg == -1)){
             $this->laud[$uusrida][$uusveerg]=$nupp;
             //echo $uusrida, $uusveerg, $nupp;
             $this->laud[$this->aktiivserida][$this->aktiivseveerg]="r";
             $this->aktiivserida=-1;
             $this->aktiivseveerg=-1;
+            $this->viimaneNupp=$nupp;
+      }else if($nupp == "m" && $this->aktiivserida - $uusrida == 2 && $this->laud[$uusrida][$uusveerg] == "r" &&
+                ($this->aktiivseveerg - $uusveerg == 2 || $this->aktiivseveerg - $uusveerg == -2) &&
+                  ($this->laud[$this->aktiivserida-1][$this->aktiivseveerg+1] == "v" || $this->laud[$this->aktiivserida-1][$this->aktiivseveerg-1] == "v")) {
+                    if($this->laud[$this->aktiivserida-1][$this->aktiivseveerg-1] == "v"){
+                      $this->laud[$this->aktiivserida-1][$this->aktiivseveerg-1]="r";
+                    }else if($this->laud[$this->aktiivserida-1][$this->aktiivseveerg+1] == "v") {
+                      $this->laud[$this->aktiivserida-1][$this->aktiivseveerg+1]="r";
+                    }
+                    $this->laud[$uusrida][$uusveerg]=$nupp;
+                    $this->laud[$this->aktiivserida][$this->aktiivseveerg]="r";
+                    $this->aktiivserida=-1;
+                    $this->aktiivseveerg=-1;
+                    $this->viimaneNupp=$nupp;
+      }else if($nupp == "v" && $this->aktiivserida - $uusrida == -2 && $this->laud[$uusrida][$uusveerg] == "r" &&
+                ($this->aktiivseveerg - $uusveerg == -2 || $this->aktiivseveerg - $uusveerg == 2) &&
+                  ($this->laud[$this->aktiivserida+1][$this->aktiivseveerg+1] == "m" || $this->laud[$this->aktiivserida+1][$this->aktiivseveerg-1] == "m")) {
+                    if($this->laud[$this->aktiivserida+1][$this->aktiivseveerg+1] == "m"){
+                      $this->laud[$this->aktiivserida+1][$this->aktiivseveerg+1]="r";
+                    }else if($this->laud[$this->aktiivserida+1][$this->aktiivseveerg-1] == "m") {
+                      $this->laud[$this->aktiivserida+1][$this->aktiivseveerg-1]="r";
+                    }
+                    $this->laud[$uusrida][$uusveerg]=$nupp;
+                    $this->laud[$this->aktiivserida][$this->aktiivseveerg]="r";
+                    $this->aktiivserida=-1;
+                    $this->aktiivseveerg=-1;
+                    $this->viimaneNupp=$nupp;
       }else{
         $this->aktiivserida=-1;
         $this->aktiivseveerg=-1;
       }
-      if($nupp != "r"){
-        $this->viimaneNupp=$nupp;
-      }
-		  //Pange tööle ja hoolitsege, et mustad saaksid käia ainult diagonaalis edasi
 	  }
 	  function t88tleURL(){
  	   if(isSet($_REQUEST["rida"])){
